2139c5cea45ce7ed95abd3cfae427411b2fc1c24
Thiru7416 <128247232+Thiru7416@users.noreply.github.com>
2023-04-28 22:50:14 -0500

    Update ex_5_2.py

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -22,5 +22,9 @@ if __name__ == "__main__":
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
 
     # Complete the data processing steps using numpy here.
-
+    data = np.loadtxt(INFILE, delimiter=',')
+    data -= np.mean(data)
+    data /= np.std(data)
+    processed = data
     # Save the output to OUTFILE using numpy routines.
+    np.savetxt(OUTFILE, processed, delimiter=',')
